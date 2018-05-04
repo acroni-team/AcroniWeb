d1c2bac3715905cb12fd067e19a3b87c0d665414
Foot-G <gustavoplm@hotmail.com>
2018-05-04 19:34:24 -0300

    Mudanças gerais no menu e botões light e dark removidos

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
+	   var c = true;
+	$(".icon-ld").click(function(){
 	
-	$("#tolight").click(function(){
-	
+       if(c == true){
 		$(this).css("color","#333");
-		$("#todark").css("color","#333");
-		$("#conteudo").css("background-color","#f2f2f2");
-		
-	});
-	$("#todark").click(function(){
-		$(this).css("color","#f2f2f2");
-		$("#tolight").css("color","#f2f2f2");
 		$("#conteudo").css("background-color","#333");
-		
+        c = false;   
+       }
+       else{
+        $(this).css("color","#333");
+		$("#conteudo").css("background-color","#f2f2f2");
+        c = true;
+        }     
+
 	});
 	
 });
+
+//	});
+//	$("#").click(function(){
+//		$(this).css("color","#f2f2f2");
+//		$("#tolight").css("color","#f2f2f2");
+//		$("#conteudo").css("background-color","#333");
+		
