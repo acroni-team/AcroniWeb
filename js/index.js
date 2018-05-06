edde2753267abdfd6c803a3d6d2c09b50b9e00c6
Foot-G <gustavoplm@hotmail.com>
2018-05-06 04:49:56 -0300

    Click na checkbox

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	var c = true;
-	$(".icon-ld").click(function(){
+	$("#icon-ld").click(function(){
 
        if(c == true){
 					$(this).css("color","#333");
@@ -25,6 +25,25 @@ $(document).ready(function(){
         }
 
 	});
+
+		$("#icon-box-dark").click(function(){
+						$("#conteudo").css("background-color","#333");
+						$("#menu").css("background-color","#730dc4");
+						$("#icon-dark").css("display","block");
+						$("#icon-box-dark").css("display","none");
+						$("#icon").css("display","none");
+						$("#icon-box").css("display","block");
+						c = false;
+		});
+		 $("#icon-box").click(function(){
+					$("#conteudo").css("background-color","#f2f2f2");
+					$("#menu").css("background-color", "#53098e");
+					$("#icon").css("display","block");
+					$("#icon-box").css("display","none");
+					$("#icon-dark").css("display","none");
+					$("#icon-box-dark").css("display","block");
+					c = true;
+			});
 
 });
 
