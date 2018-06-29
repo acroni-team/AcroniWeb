aa1f776a1b77e1a5a6f14cfd23a17bb4616dc7b1
Gustavo Palma <gustavo.palma.gp@gmail.com>
2018-06-28 22:58:43 -0300

    projeto cadastro e login sem design

---
+++ CadastroLogin/CadastroLogin/Cadastro.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CadastroLogin
+{
+    public partial class Cadastro : System.Web.UI.Page
+    {
+        Valida v = new Valida();
+        ClasseConexao xx = new ClasseConexao();
+        DataSet ds = new DataSet();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            bool val;
+            if (txtNome.Text == "")
+                val = false;
+            else if (v.validarEmail(txtEmail.Text) == false)
+                val = false;
+            else if (v.validarCPF(txtCPF.Text) == false)
+                val = false;
+            else if (v.validarData(txtData.Text) == false)
+                val = false;
+            else if (txtRG.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtEndereco.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtCidade.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtCEP.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtUF.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtTelefone.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtUsu.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtPass.Text.Replace(" ", "") == "" || txtPass2.Text.Replace(" ", "") == "")
+                val = false;
+            else if (txtPass.Text != txtPass2.Text)
+                val = false;
+            else
+                val = true;
+
+            try
+            {
+                if (val)
+                {
+                    xx = new ClasseConexao();
+                    ds = new DataSet();
+                    xx.executa_sql("INSERT INTO tblCliente VALUES ('" + txtUsu.Text + "','" + txtPass.Text + "','" + txtNome.Text + "','" + txtEmail.Text + "','" + txtCPF.Text + "','" + txtData.Text + "','" + txtRG.Text + "','" + txtEndereco.Text + "','" + txtCidade.Text + "','" + txtCEP.Text + "','" + txtUF.Text + "','" + txtTelefone.Text + "')");
+                    Response.Redirect("Login.aspx");
+                }
+                else
+                    lblErro.Text = "Campo inválido";
+            }
+            catch { }
+        }
+
+        
+    }
+}
---
+++ CadastroLogin/CadastroLogin/Login.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="CadastroLogin.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Usuário"></asp:Label>
+        <br />
+        <div>
+            <asp:TextBox ID="txtUser" runat="server"></asp:TextBox>
+        </div>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Senha"></asp:Label>
+        </p>
+        <p>
+            <asp:TextBox ID="txtPass" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="lblErro" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="btnEntrar" runat="server" OnClick="btnEntrar_Click" Text="Entrar" />
+            <asp:Button ID="btnRedirect" runat="server" OnClick="btnRedirect_Click" Text="Cadastrar" />
+        </p>
+    </form>
+</body>
+</html>
