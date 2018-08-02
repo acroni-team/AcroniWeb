4e6ce67b5c8bc737072f09b1f4ec8edb0e17ee36
FOOTI <37518682+Foot-G@users.noreply.github.com>
2018-08-01 21:29:07 -0300

    mandei pro design dark e arrumei uns bugs na animacao

--- Novo Cadastro/js/cadastro.js
+++ Novo Cadastro/js/cadastro.js
@@ -3,7 +3,7 @@
        // $(".alo:nth-child(1)").removeClass("animacao");
       //  $(".alo:nth-child(1)").addClass("animacao2");
        // $(".alo:nth-child(1)").addClass("animacao");
-   // }); 
+   // });
 //});
 
 
@@ -25,22 +25,22 @@ $(document).ready(function () {
 
     function animate(alo) {
         if(i == 0 || i == 1) {
-            $(alo).eq(0).addClass("animacao2"); //remove o 1
-            $(alo).eq(1).addClass("animacao");  //adiciona o 2
+            $(alo).eq(0).addClass("some"); //remove o 1
+            $(alo).eq(1).addClass("aparece").focus();  //adiciona o 2
             i = i + 1;
         } else if(i == 2 || i == 3) {
-            $(alo).eq(1).addClass("animacao2"); //remove o 2
-            $(alo).eq(2).addClass("animacao");  //adiciona o 3
+            $(alo).eq(1).addClass("some"); //remove o 2
+            $(alo).eq(2).addClass("aparece").focus();  //adiciona o 3
             i = i + 1;
         }
     }
-    
+
     $(".bluev2").click(animateBoth);
-    
+
     $(document).keypress(function(e) {
         if(e.which == 13) {
             animateBoth();
         }
     });
-    
+
 });
