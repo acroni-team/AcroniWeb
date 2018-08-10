f7a8ac350e9965cc980e7fe150a46fac3c96f7df
FOOTI <37518682+Foot-G@users.noreply.github.com>
2018-08-10 00:21:53 -0300

    Adicionei o projeto AcroniWeb-4.5 que agora funciona nos PCs da escola e começei a organizar o css,js etc

---
+++ AcroniWeb-4.5/AcroniWeb-4.5/construct.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/layout.Master" AutoEventWireup="true" CodeBehind="construct.aspx.cs" Inherits="AcroniWeb.img.construct" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <link rel="stylesheet" type="text/css" href="css/area-restrita.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css" />
+    <link rel="stylesheet" type="text/css" href="css/default.css" />
+    
+</asp:Content>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <section>
+        <div>
+            <h1>Em Breve</h1>
+            <p>Estamos trabalhando para construir essa página</p>
+            <asp:TextBox id="txtEmail" style="width:500px;margin-top:30px;" class="caixxinha" type="text" placeholder="Entre com seu email e seja notificado" name="notifica" runat="server"></asp:TextBox>
+            <asp:Button class="button" type="button" Text="Enviar" runat="server" OnClick="Unnamed1_Click"/>
+        </div>
+    </section>
+
+</asp:Content>
+
---
+++ AcroniWeb-4.5/AcroniWeb-4.5/loja.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AcroniWeb
+{
+    public partial class loja : System.Web.UI.Page
+    {
+        SqlConnection conexao_sql = new SqlConnection("Data Source = " + Environment.MachineName + "\\SQLEXPRESS" + ";Initial Catalog = Acroni_SQL; User ID = Acroni; Password = acroni7");
+        DataSet ds;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conexao_sql.State != ConnectionState.Open)
+                    conexao_sql.Open();
+                String select = "SELECT * FROM tblProdutos";
+                SqlDataAdapter da = new SqlDataAdapter(select, conexao_sql);
+                ds = new DataSet();
+                da.Fill(ds);
+                DataList1.DataSource = ds.Tables[0];
+                DataList1.DataBind();
+
+                conexao_sql.Close();
+            }
+            catch (Exception ex)
+            {
+                conexao_sql.Close();
+            }
+        }
+    }  
+}
