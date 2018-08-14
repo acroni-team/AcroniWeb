c68344034afd3b301afaaebb4df6127050f6792c
Gustavo Palma <gustavo.palma.gp@gmail.com>
2018-08-13 21:29:35 -0300

    Cálculo de frete com o compilador da escola grr

---
+++ CalculoFreteV2/CalculoFreteV2/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CalculoFreteV2._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtCep" AutoPostBack="true" runat="server" OnTextChanged="txtCep_TextChanged"></asp:TextBox>
+    </div>
+        <p>
+        <asp:Label ID="lblPreco" runat="server" Text=""></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ CalculoFreteV2/CalculoFreteV2/default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculoFreteV2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtCep_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCep.Text.Length == 8)
+            {
+                var correios = new WSCorreiosFrete.CalcPrecoPrazoWSSoapClient();
+                var resposta = correios.CalcPrecoPrazo(string.Empty, string.Empty, "40010", "01101010", txtCep.Text, "6", 1, 20, 10, 26, 0, "N", 200, "N");
+                var respostaReal = resposta.Servicos.FirstOrDefault();
+                if (Convert.ToInt16(respostaReal.PrazoEntrega) > 1)
+                    lblPreco.Text = respostaReal.Valor + " em " + respostaReal.PrazoEntrega + " dias";
+                else
+                    lblPreco.Text = respostaReal.Valor + " em " + respostaReal.PrazoEntrega + " dia";
+            }
+        }
+    }
+}
