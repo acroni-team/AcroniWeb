497e94bf352d56fc2fefda659ca1d9bed788b192
FOOTI <37518682+Foot-G@users.noreply.github.com>
2018-10-04 21:08:23 -0300

    Galeria agora puxa as coleções

--- AcroniWeb-4.5/galeria.aspx.cs
+++ AcroniWeb-4.5/galeria.aspx.cs
@@ -13,48 +13,34 @@ namespace AcroniWeb_4._5
 {
     public partial class galeria : System.Web.UI.Page
     {
-        SqlConnection conexao_SQL = new SqlConnection(acroni.classes.Conexao.nome_conexao);
+        
         DataSet ds;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["logado"].ToString() != "1")
-                Response.Redirect("default.aspx");
-
-
-
-            try
+            using (SqlConnection conexao_SQL = new SqlConnection(acroni.classes.Conexao.nome_conexao))
             {
-                if (conexao_SQL.State != ConnectionState.Open)
-                    conexao_SQL.Open();
-                String select = "SELECT * FROM tblColecao";
-                SqlDataAdapter da = new SqlDataAdapter(select, conexao_SQL);
-                ds = new DataSet();
-                da.Fill(ds);
-
-
-                foreach (DataRow row in ds.Tables[0].Rows)
+                try
                 {
+                    if (conexao_SQL.State != ConnectionState.Open)
+                        conexao_SQL.Open();
+                    String select = "SELECT imagem_coleção FROM tblColecao WHERE usuario ='" + Session["usuario"] + "'";
 
-                    //Get the byte array from image file
-                    byte[] imgBytes = (byte[])row["image_colecao"];
-
-                    //If you want convert to a bitmap file
-                    TypeConverter tc = TypeDescriptor.GetConverter(typeof(Bitmap));
-                    Bitmap MyBitmap = (Bitmap)tc.ConvertFrom(imgBytes);
-
-
-                    string imgString = Convert.ToBase64String(imgBytes);
-                    //Set the source with data:image/bmp
-                    imgColecao.ImageUrl = String.Format("data:image/Bmp;base64,{0}\"", imgString);   //img is the Image control ID
+                    using (SqlCommand comando_SQL = new SqlCommand(select, conexao_SQL))
+                    {
+                        using (SqlDataReader tabela = comando_SQL.ExecuteReader())
+                        {
+                            tabela.Read();
+                            byte[] imgBytes = (byte[])tabela[0];
+                            string imgString = Convert.ToBase64String(imgBytes);
+                            imgColecao.ImageUrl = "data:image/jpg;base64," + imgString;
+                        }
+                    }
                 }
-
-                conexao_SQL.Close();
+                catch (Exception ex)
+                {
+                    conexao_SQL.Close();
+                }
             }
-            catch (Exception ex)
-            {
-                conexao_SQL.Close();
-            }
-
         }
     }
 }
