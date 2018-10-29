445f276c332c16fea09b2115be9170cf41d0a29c
Gravata Prata <guimotapereira25@gmail.com>
2018-10-29 14:28:41 -0300

    Pé: Adicionei as coleções da galeria

--- AWP/AcroniWeb-4.5/galeria.aspx
+++ AWP/AcroniWeb-4.5/galeria.aspx
@@ -2,9 +2,14 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">  
-    <div class="right right-logado right-galeria">
-            <a href="colecao.aspx"><asp:Image ID="imgColecao" class="colecao-picture" runat="server" ImageUrl='' /></a>
+    <div class="right right-logado right-galeria colecao">
+        <div class="galeria-header">
+            <h1>Seja bem-vindo, </h1><asp:Label ID="Nome" class="nome" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <a href="colecao.aspx"><asp:Image ID="imgColecao" class="img-colecao" runat="server" ImageUrl='' /></a>
             <asp:Image ID="imgStatus" class="galeria-status" runat="server"  ImageUrl='img/galeria-vazia.png' />
+        </div>
     </div>
       <script>
         document.getElementById("galeria").classList.add("active");
--- AWP/AcroniWeb-4.5/galeria.aspx.cs
+++ AWP/AcroniWeb-4.5/galeria.aspx.cs
@@ -17,25 +17,33 @@ namespace AcroniWeb_4._5
         DataSet ds;
         protected void Page_Load(object sender, EventArgs e)
         {
+            Nome.Text = Session["usuario"].ToString(); 
+
             using (SqlConnection conexao_SQL = new SqlConnection(acroni.classes.Conexao.nome_conexao))
             {
                 try
                 {
                     if (conexao_SQL.State != ConnectionState.Open)
                         conexao_SQL.Open();
-                    String select = "SELECT imagem_coleção FROM tblColecao WHERE usuario ='" + Session["usuario"] + "'";
+                    String select = "SELECT imagem_colecao FROM tblColecao AS colec INNER JOIN tblCliente AS cli ON cli.id_cliente = colec.id_cliente AND usuario ='" + Session["usuario"] + "'";
 
                     using (SqlCommand comando_SQL = new SqlCommand(select, conexao_SQL))
                     {
                         using (SqlDataReader tabela = comando_SQL.ExecuteReader())
                         {
                             tabela.Read();
-                            byte[] imgBytes = (byte[])tabela[0];
-                            string imgString = Convert.ToBase64String(imgBytes);
-                            imgColecao.ImageUrl = "data:image/jpg;base64," + imgString;
+                            if (tabela.HasRows)
+                            {
+                                byte[] imgBytes = (byte[])tabela[0];
+                                string imgString = Convert.ToBase64String(imgBytes);
+                                imgColecao.ImageUrl = "data:image/jpg;base64," + imgString;
+                                imgStatus.Attributes.Add("style", "display:none");
+                            }
 
                         }
                     }
+
+                    
                 }
                 catch (Exception ex)
                 {
